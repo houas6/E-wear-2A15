cc6ac22b23dbff4c80804ad5444c06102a2b38b5
houasaziz <medaziz.houas@esprit.tn>
2022-05-04 16:05:24 +0100

    metiers done

--- Views/script.js
+++ Views/script.js
@@ -1,83 +1,90 @@
 function verif() {
 
-  var nom = document.forms["Form"]["nom"].value;
-  var prix = document.forms["Form"]["prix"].value;
-  var telephone = document.forms["Form"]["tel"].value;
-  var adresse = document.forms["Form"]["adresse"].value;
-  var errorN = document.getElementById('errorNR');
-  var errorprix = document.getElementById('errorMR');
-  var errortel = document.getElementById('errortel');
-  var erroradresse = document.getElementById('erroradresse');
-  var er1=1;
-  var er2=1;
-  var er3=1;
-  var er4=1;
-  var letters = /^[A-Za-z]+$/;
- 
+    var nom = document.forms["Form"]["nom"].value;
+    var telephone = document.forms["Form"]["telephone"].value;
+    var adresse = document.forms["Form"]["adresse"].value;
+    var prix = document.forms["Form"]["prix"].value;
+    
+    
+    var errornom = document.getElementById('errornom');
+    var errortelephone = document.getElementById('errortelephone');
+    var erroradresse = document.getElementById('erroradresse');
+    var errorprix = document.getElementById('errorprix');
+    var er1 = 1;
+    var er2 = 1;
+    var er3 = 1;
+    var er4 = 1;
+    
+    var letters = /^[A-Za-z]+$/;
+    
 
-  if (nom == "") {
-      errorN.innerHTML = "Veuillez entrer votre nom!";
-      
-  }
-  else if (!(nom.match(letters) && nom.charAt(0).match(/^[A-Z]+$/))) {
-      errorN.innerHTML = "Veuillez entrer un nom valid!";
-  } else {
-      errorN.innerHTML = "";
-      er1=0;
-  }
+    if (nom == "") {
+        errornom.innerHTML = "Veuillez entrer votre nom!";
 
-  if (prix == "") {
-      errorprix.innerHTML = "Veuillez entrer votre prix!";
+    }
+    else if (!(nom.match(letters) && nom.charAt(0).match(/^[A-Z]+$/))) {
+        errornom.innerHTML = "Veuillez entrer un nom valid!";
+    } else {
+        errornom.innerHTML = "";
+        er1 = 0;
+    }
 
-  }
-  else if (!prix.match()) {
-      errorprix.innerHTML = "Veuillez entrer un prix valid!";
-  }
-  else {
-      errorprix.innerHTML = "/[0-9]/";
-      er2=0;
-  }
-
-  if (telephone == "") {
-      errortel.innerHTML = "Veuillez entrer votre mot de passe!";
-
-  }
-  else if (!(telephone.match(/[1-8]/g) 
    
-     ( telephone.length == 8)) {
-      errortel.innerHTML = "le telephone doit contenir 8 num";
 
-  }
-  else {
-      errortel.innerHTML = "";
-      er3=0;
-  }
+    if (telephone == "") {
+        errortelephone.innerHTML = "Veuillez entrer numero de telephone!";
 
-  if (adresse == "") {
-      erroradresse.innerHTML = "Veuillez entrer votre adress!";
+    }
+    else if (!(telephone.match(/[1-9]/) 
+        &&
+        telephone.length >= 8)){
+        errortelephone.innerHTML = "Le mot de telephone doit contenir 8 numbers";
 
-  }
-  else if (!adresse.match(",tunis")) {
-      erroradresse.innerHTML = "Veuillez entrer une adress qui se termine avec ,tunis";
-  }
-  else {
-      erroradresse.innerHTML = "";
-      er4=0;
-  }
+    }
+    else {
+        errortelephone.innerHTML = "";
+        er2 = 0;
+    }
 
-  if(er1== 1 || er2==1 || er3== 1 || er4== 1 ){
-      return 1;
-  }else{
-      return 0;
-  } 
+    if (adresse == "") {
+        erroradresse.innerHTML = "Veuillez entrer votre adress!";
+
+    }
+    else if (!adresse.match(",tunis")) {
+        erroradresse.innerHTML = "Veuillez entrer une adress qui se termine avec ,tunis";
+    }
+    else {
+        erroradresse.innerHTML = "";
+        er3 = 0;
+    }
+
+    if (prix == "") {
+        errorprix.innerHTML = "Veuillez entrer votre prix!";
+
+    }
+    else if (!prix.match(/[0-9]/)) {
+        errorprix.innerHTML = "Veuillez entrer prix";
+    }
+    else {
+        errorprix.innerHTML = "";
+        er4 = 0;
+    }
+
+    
+
+
+    if (er1 == 1 || er2 == 1 || er3 == 1 || er4 == 1 ) {
+        return 1;
+    } else {
+        return 0;
+    }
 
 
 }
 
 function validateForm(e) {
- if (verif()==1)
- {
-  e.preventDefault();
-  }
-  
+    if (verif() == 1) {
+        e.preventDefault();
+    }
+
 };
